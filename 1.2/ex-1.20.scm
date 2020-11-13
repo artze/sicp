4bcc4027b1d448b6e744d0b23107e1d750261eb2
artze <13703523+artze@users.noreply.github.com>
2020-11-13 22:15:49 +0800

    Add fermat-test

---
+++ 1.2/ex-1.20.scm
@@ -0,0 +1,4 @@
+; Normal Order Evaluation: remainder called 18 times
+; Applicative Order Evaluation: remainder called 4 times
+
+; Details here: https://codology.net/post/sicp-solution-exercise-1-20/
