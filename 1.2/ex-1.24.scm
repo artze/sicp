f7cf6e349f650b21ea7aac119333bfe31cefec72
artze <13703523+artze@users.noreply.github.com>
2020-11-15 14:23:10 +0800

    Complete ex-1.24

--- 1.2/ex-1.24.scm
+++ 1.2/ex-1.24.scm
@@ -42,7 +42,7 @@
   (start-prime-test n (runtime)))
 
 (define (start-prime-test n start-time)
-  (if (fast-prime? n 2000)
+  (if (fast-prime? n 4000)
     (report-prime (- (runtime) start-time))
     (print "not prime")
   )
@@ -81,3 +81,21 @@
 (timed-prime-test 1000000000000037)
 (timed-prime-test 1000000000000091)
 (timed-prime-test 1000000000000159)
+
+; With order of growth of O(log n) we would expect the elapsed time
+; to increase at a fixed amount with every increment of n in 1x10^n.
+
+; Results below:
+;             millis  millis  millis    avg               time increase
+; 1x10^10     297     283     289       289.6666666667    
+; 1x10^11     324     326     334       328               38.3333333333
+; 1x10^12     338     341     365       348               20
+; 1x10^13     385     364     380       376.3333333333    28.3333333333
+; 1x10^14     403     393     406       400.6666666667    24.3333333333
+; 1x10^15     411     436     426       424.3333333333    23.6666666667
+
+; Results above shows that the time increase is roughly the same at each
+; 10x increment. Also note that fast-prime? could be slower for smaller 
+; primes compared to prime? implementation in (ex-1.23). The advantage of
+; fast-prime? can be seen only for larger primes. See below:
+; https://codology.net/post/sicp-solution-exercise-1-24/
